8b2af35b762dcb26835abb62f3125b1d51ceb354
Marcin Pilarczyk <marcinpilarczyk@me.com>
2018-08-27 17:01:22 +0200

    Pass schema to middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require('http');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3800;
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema
+	})
+);
 
 server.listen(PORT, () => {
 	console.log(`Listening on for request on port ${PORT}`);
